e015f28773e08f849d4b3e879243555b6c57ec12
Muhammad Zeeshan <m.zeeshanchandia1@gmail.com>
2025-05-10 15:01:03 +0500

    fask interface

--- application.py
+++ application.py
@@ -8,9 +8,32 @@ from sklearn.preprocessing import StandardScaler
 application = Flask(__name__)
 app=application
 
+# import ridge regressor and standartscaler
+ridge_regressor=pickle.load(open(r'D:\Python Projects\algeraian forest fires\models\RidgeRegressor.pkl','rb'))
+standard_scaler=pickle.load(open(r"D:\Python Projects\algeraian forest fires\models\StandardScaler.pkl",'rb'))
+
 @app.route('/')
-def hello():
-    return "Hello Army"
+def index():
+    return render_template('home.html')
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='POST':
+         # Retrieve form data and convert to float
+        Temperature = float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
+        new_data_scaled=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge_regressor.predict(new_data_scaled)
+        return render_template('home.html',results=result[0])
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
